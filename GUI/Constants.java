cce414ed4868992625a4aad5003d03af15544355
Qiongao Liu (FA Talent) <v-qiongaoliu@microsoft.com>
2021-12-07 17:37:44 +0800

    pause video available

--- GUI/Constants.java
+++ GUI/Constants.java
@@ -3,5 +3,6 @@ package GUI;
 public class Constants {
     static final int MaxFrameNumber = 9000;
     static final int FramesPerSecond = 30;
-    static final int EXTERNAL_BUFFER_SIZE = 524288;
+//    static final int EXTERNAL_BUFFER_SIZE = 524288;
+    static final int EXTERNAL_BUFFER_SIZE = 8820;
 }
