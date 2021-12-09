3f14725153d206b7729cb61bdd55f5cfa2a56d74
Qiongao Liu (FA Talent) <v-qiongaoliu@microsoft.com>
2021-12-09 20:27:08 +0800

    if cover choose first

--- GUI/VideoPlayer.java
+++ GUI/VideoPlayer.java
@@ -538,6 +538,8 @@ class DrawPlayer extends JLabel implements MouseListener {
 
                         File videoDir = Paths.get(VideoPlayer.videoDir.getParent()).resolve(sndVideoName).toFile();
                         VideoPlayer.Replay(sndVideoFrame, videoDir);
+
+                        return;
                     }
                 }
             }
